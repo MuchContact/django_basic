1057a0a83120eddb764ae4a0adcc602be8befcc2
hjliu <hjliu@thoughtworks.com>
2016-01-10 23:38:20 +0800

    transaction

--- django_basic/urls.py
+++ django_basic/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from fields import views
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'fields.views.app_list', name='apps'),
+    url(r'^$', views.app_list, name='apps'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
--- fields/views.py
+++ fields/views.py
@@ -6,6 +6,8 @@ from fields.models import App
 from fields.serializers import AppSerializer
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
+from django.db import transaction
+from rest_framework.decorators import api_view
 
 def index(request):
     return HttpResponse(u"欢迎光临 自强学堂!")
@@ -20,6 +22,8 @@ class JSONResponse(HttpResponse):
         super(JSONResponse, self).__init__(content, **kwargs)
 
 @csrf_exempt
+@transaction.atomic
+@api_view(['GET', 'POST'])
 def app_list(request):
     """
     List all code apps, or create a new app.
@@ -30,9 +34,10 @@ def app_list(request):
         return JSONResponse(serializer.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = AppSerializer(data=data)
+        sid = transaction.savepoint()
+        serializer = AppSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
+            transaction.savepoint_rollback(sid)
             return JSONResponse(serializer.data, status=201)
         return JSONResponse(serializer.errors, status=400)
